64a84ac275576ee02ee95eff150bbc2fdf786957
Naomi <naomistrall@hotmail.com>
2023-08-02 02:31:11 -0300

    Implementação da rota post /urls/shorten

---
+++ src/controllers/urls.controllers.js
@@ -0,0 +1,24 @@
+import { nanoid } from "nanoid";
+import { db } from "../database/database.connection.js";
+
+export async function shortener(req, res) {
+  const { url } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const shortUrl = nanoid(8);
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const { userId } = (await db.query(`SELECT * FROM sessions WHERE token=$1`, [token])).rows[0];
+
+    const { rows } = await db.query(
+      `INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3) RETURNING id, "shortUrl"`,
+      [userId, url, shortUrl]
+    );
+
+    res.status(201).send(rows[0]);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { urlSchema } from "../schemas/urls.schemas.js";
+import { shortener } from "../controllers/urls.controllers.js";
 
 const urlsRoutes = Router();
+
+urlsRoutes.post("/urls/shorten", validateSchema(urlSchema), shortener);
 
 export default urlsRoutes;
